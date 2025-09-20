ccdde2ef20c3da185f4de76241b4dff4f4402d7e
Tornike Mikhelidze <tornike.mixelidze@gmail.com>
2025-09-20 17:35:42 +0400

    how to use Is operator

---
+++ GettingStartedWithC#/GettingStartedWithC#/IsOperator.cs
@@ -0,0 +1,15 @@
+﻿
+/*
+    Example of using the 'is' operator in C#
+ */
+namespace GettingStartedWithC
+{
+    class IsOperator
+    {
+        static void Main(string[] args)
+        {
+            bool result=123 is int; // true
+            Console.WriteLine(result);
+        }
+    }
+}
